883c2b0befe3bfaca17397be6ef6ff5f16e5981d
Glatcher <georkuchiev@mail.ru>
2025-11-30 13:32:40 +0300

    Bump version to 0.2.3

--- bytesto4t/src-tauri/src/mcp/server.rs
+++ bytesto4t/src-tauri/src/mcp/server.rs
@@ -3,7 +3,7 @@ use tauri::AppHandle;
 use crate::mcp::cmd;
 
 pub async fn start(app_handle: AppHandle) -> McpResult<()> {
-    let mut server = McpServer::new("bytesto4t".to_string(), "0.2.2".to_string());
+    let mut server = McpServer::new("bytesto4t".to_string(), "0.2.3".to_string());
 
     cmd::get_function_list::register(&mut server, app_handle.clone()).await?;
     cmd::load_bytecode::register(&mut server, app_handle.clone()).await?;
